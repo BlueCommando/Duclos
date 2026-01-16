0a4c3078e4c0c18dacf3e46e0f2c891fa178213d
BlueCommando <tyleryellz@gmail.com>
2026-01-16 16:56:20 -0500

    text input

--- app/modes/index.tsx
+++ app/modes/index.tsx
@@ -1,13 +1,17 @@
 import { createChatStyle } from "@/assets/styles/chat.style";
+import InputText from "@/components/chat/inputText";
 import useTheme from "@/hooks/useTheme";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from 'expo-linear-gradient';
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const colors = useTheme()
   const chatStyle = createChatStyle(colors)
+
+  const handleMsg = (msg: string) => {
+    console.log(msg)
+  }
 
   return (
     <LinearGradient 
@@ -17,6 +21,7 @@ export default function Index() {
       <SafeAreaView>
         <Text>Edit app/index.tsx to edit this screen.</Text>
         <Text>I'm just like wally west!</Text>
+        <InputText onSend={handleMsg} />
       </SafeAreaView>
     </LinearGradient>
   );
