4bc2921e9a23b505f09567081e793aa602180895
BlueCommando <tyleryellz@gmail.com>
2026-03-20 22:02:08 -0400

    working camera

--- app/modes/imagery.tsx
+++ app/modes/imagery.tsx
@@ -1,32 +1,43 @@
 import { createImageryStyle } from '@/assets/styles/modes/imagery.style'
+import CameraView from '@/components/imagery/CameraView'
 import useTheme from '@/hooks/useTheme'
 import { LinearGradient } from 'expo-linear-gradient'
-import React from 'react'
+import React, { useRef } from 'react'
 import { TouchableOpacity, View } from 'react-native'
+import { Camera } from 'react-native-vision-camera'
 
 // TODO:
 //
+// Display camera ✅
 // Get picture from user camera ❌
 // allow user to crop image ❌
 // tell user if they want to pre-input a message for the image ❌
 // try react-native-markdown-display for the ai... ❌
 //
 
-// Help with react-native-image-picker:
-// https://github.com/react-native-image-picker/react-native-image-picker/blob/main/README.md
+// Help with react-native-vision-camera:
+// https://react-native-vision-camera.com/docs/api
 
 const imagery = () => {
+  const camera = useRef<Camera | null>(null)
+
+  const takePicture = async () => {
+    
+  };
+
   const theme = useTheme();
   const style = createImageryStyle(theme);
 
   return (
     <LinearGradient style={style.container} colors={theme.gradients.background}>
+      <CameraView camera={camera}/>
+
       <View style={style.bottomCenterView}>
         <View style={style.pictureBar}>
 
           <View style={style.pictureButtonBorder1}>
             <View style={style.pictureButtonBorder2}>
-              <TouchableOpacity style={style.pictureButton}/>
+              <TouchableOpacity onPress={takePicture} style={style.pictureButton}/>
             </View>
           </View>
 
---
+++ components/imagery/CameraView.tsx
@@ -0,0 +1,55 @@
+import { useFocusEffect } from '@react-navigation/native';
+import React, { Ref } from 'react';
+import { Alert, Linking, StyleSheet } from 'react-native';
+import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
+
+type CameraView = {
+  camera: Ref<Camera> | null
+}
+
+let state = 0;
+
+const cameraView = ({ camera }: CameraView) => {
+  const { hasPermission, requestPermission } = useCameraPermission()
+
+  const device = useCameraDevice('back')
+
+  if (device == null) return
+
+  const accessCameraPerms = async () => {
+    if (hasPermission) return;
+
+    const perm = await requestPermission();
+    
+    if (perm){
+      state++;
+    } else {
+      Alert.alert(
+        "Camera Permission Required",
+        "Please enable camera access in Settings.",
+        [
+          {text: "OK", onPress: () => Linking.openSettings()},
+          {text: "Cancel"},
+        ]
+      );
+    }
+  };
+
+  useFocusEffect(() => {
+    setTimeout(() => accessCameraPerms(), 500);
+  });
+
+  return (
+    hasPermission && (
+        <Camera 
+          style={StyleSheet.absoluteFill}
+          ref={camera} 
+          device={device}
+          isActive={true}
+          key={state}
+        />
+      )
+    )
+}
+
+export default cameraView
